879413e83a02fec02fc8cf0e387d6c5c8a5f0dd7
ngocanh <nggocanh.nguyen@gmail.com>
2024-06-24 08:01:22 +0700

    add file gen_image

--- generated_image/cgan_generated.py
+++ generated_image/cgan_generated.py
@@ -1,26 +1,10 @@
-from models.cgan_model import generator 
-import torch 
-import util.utils as utils
+import torch
 import numpy as np
-import os 
-import argparse
+import os
+import util.utils as utils
 
-
-def GenOptions():
-    parser = argparse.ArgumentParser(description='some params for generating image')
-    parser.add_argument('--weight', type=str, default='checkpoints/plantvillage_cgan/cganlatest_G.pkl',
-                         help='path to the generator weight')
-    parser.add_argument('--zdim-in', type=int, default=62, help='number of z channel for input dim')
-    parser.add_argument('--input-size', type=int, default=62, help='size of input')
-    parser.add_argument('--classes', type=int, default=15, help='number of classes of the dataset')
-    parser.add_argument('--zdim-out',type=int, default=3, help='number of classes of the output dim')
-    parser.add_argumnet('--batch', type=int, default=225, help='batch size during generation')
-    parser.add_argument('--path-save', type='str', default='results', help='path save inference image')
-    parser.add_argument('--nimage', type=int, default=50000, help='number of image generated')
-    return parser 
-
-
-def gen_image(model, opt) -> None:
+def gen_image(model, opt, device) -> None:
+    
     os.makedirs(opt.path_save, exist_ok=True)
     num_iter = opt.nimage // opt.batch + 1
 
@@ -38,18 +22,4 @@ def gen_image(model, opt) -> None:
             utils.save_images(sample[np.newaxis, ...], [1, 1],
                             f'{opt.path_save}/test_{j+i}.png')
 
-if __name__ == '__main__':
-    opt = GenOptions().parse()
-    
-    # check device 
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    
-    # load model 
-    assert os.path.isfile(opt.weight), f'{opt.weight} is not a valid file'
-    state_dict = torch.load(opt.weight)
-    model = generator(input_dim=opt.zdim_in, output_dim=opt.zdim_out, 
-                      input_size=opt.input_size, class_num=opt.classes)
-    model.load_state_dict(state_dict).to(device)
-    model.eval()
-    
-    gen_image(model=model, opt=opt)
+
