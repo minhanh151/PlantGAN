66ede654156ae81cc43ae3862ef1e6dd51c406d7
minhanh151 <minhanh.pham151@gmail.com>
2024-07-04 17:36:43 +0100

    update results

--- gen_image.py
+++ gen_image.py
@@ -13,13 +13,14 @@ def GenOptions():
     parser = argparse.ArgumentParser(description='some params for generating image')
     parser.add_argument('--weight', type=str, default='checkpoints/plantvillage_cgan/cganlatest_G.pkl',
                          help='path to the generator weight')
-    parser.add_argument('--zdim-in', type=int, default=62, help='number of z channel for input dim')
+    parser.add_argument('--zdim-in', type=int, default=62, 
+                        help='number of z channel for input dim, 32 for cvae and vae')
     parser.add_argument('--input-size', type=int, default=64, help='size of input')
     parser.add_argument('--classes', type=int, default=15, help='number of classes of the dataset')
     parser.add_argument('--zdim-out',type=int, default=3, help='number of classes of the output dim')
     parser.add_argument('--batch', type=int, default=225, help='batch size during generation')
     parser.add_argument('--path-save', type=str, default='results', help='path save inference image')
-    parser.add_argument('--niter', type=int, default=223, help='number of iteration generated')
+    parser.add_argument('--niter', type=int, default=45, help='number of iteration generated')
     parser.add_argument('--type', type=str, default='vae', help='type of model to infer')
 
     return parser 
